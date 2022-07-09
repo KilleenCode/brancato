60bc4f3c48de9b1f402155f3b4f6cbc3a6157101
Ryan Killeen <rykilleen@gmail.com>
2022-07-09 11:15:32 -0400

    fix: position omnibar on primary monitor on focus

--- src-tauri/src/windows.rs
+++ src-tauri/src/windows.rs
@@ -2,6 +2,21 @@ use tauri::{AppHandle, Manager};
 
 pub fn focus_window(app: &AppHandle, label: String) -> Result<(), tauri::Error> {
   let window = app.get_window(&label).unwrap();
+
+  let main_monitor = window
+    .primary_monitor()
+    .expect("Couldn't get primary monitor");
+
+  match main_monitor {
+    Some(monitor) => {
+      window
+        .set_position(*monitor.position())
+        .expect("Couldn't set position");
+      window.center().expect("Failed to center")
+    }
+    _ => (),
+  }
+
   window.show()?;
   window.unminimize()?;
   window.set_focus()?;
